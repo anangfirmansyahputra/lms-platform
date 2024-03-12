301c5176de758ad6bbedaa1cfb6e27038644417b
anangfirmansyah <anangfirmansyahp5@gmail.com>
2024-03-12 16:05:11 +0800

    dynamic layout

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,34 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
 
 export default function NavbarRoutes() {
+  const pathName = usePathname();
+
+  const isTeacherPage = pathName?.startsWith("/teacher");
+  const isPlayerPage = pathName?.includes("/chapter");
+
   return (
     <div className="ml-auto flex gap-x-2">
-      <UserButton />
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="mr-2 h-4 w-4" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
