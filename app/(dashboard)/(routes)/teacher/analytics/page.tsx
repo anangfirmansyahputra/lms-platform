7711693f27e1652b88b2d47306db317862530fb5
anangfirmansyah <anangfirmansyahp5@gmail.com>
2024-03-23 15:08:25 +0800

    teacher analytics

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,3 +1,23 @@
-export default function AnalyticsPage() {
-  return <div>AnalyticsPage</div>;
+import { getAnalytics } from "@/actions/get-analytics";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import DataCard from "./_components/data-card";
+import Chart from "./_components/chart";
+
+export default async function AnalyticsPage() {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+  return (
+    <div className="p-6">
+      <div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+      <Chart data={data} />
+    </div>
+  );
 }
